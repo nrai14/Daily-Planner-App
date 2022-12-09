a68597bdf6c1eb9fd532854327bb98b246202cf4
nrai14 <nishadrai14@gmail.com>
2022-12-09 15:05:14 +0000

    pseudo coding

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+// current time will be highlighted in red 
+// past time will be highlighted in gray
+// available time will be highlighted in green 
+
+// each time box is an input (you can type into it)
+
+// once task has been written, save it (to local storage) (feedback message?)
+
+// e.g. 12 - Take lunch --> Save to local storage --> Save 
+
+//
