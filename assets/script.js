214ee12aab7b4139f1ae7f10e276cd291d922780
nrai14 <nishadrai14@gmail.com>
2022-12-13 23:44:48 +0000

    changed readme

--- assets/script.js
+++ assets/script.js
@@ -13,5 +13,31 @@ NOtes: Hours = object?
 
 */
 
-var currentDate = moment().format("MMM Do, YYYY");
+
+// Current date: Day Time
+
+var currentDate = moment().format("dddd, MMMM YYYY");
 $("#currentDay").text(currentDate);
+
+
+// for every hour from 9 to 5, create a time block row 
+// build time blocks with loop that loops at 9 and breaks at 6
+// for each loop generate or build html time block
+    // append timeblock to container
+        // Hour
+            // A number corresponding with the hour in 12 hour format
+        // Textarea
+            // Show existing event text, if any and allow user to input event text
+        // save button
+            // when clicked, store/reset the event corresponding with the hour to localStorage 
+    // increase hour by one
+    // check if hour is past, current or future and apply correspending css class 
+
+
+// var time = 0; 
+
+// while (time < 9; time++) {
+    // create three rows
+    // moment js to add time slot ++hour
+    // add texterea and button 
+
